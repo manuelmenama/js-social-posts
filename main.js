0dd095e279697d8419ec21da786a35af3d2d8163
manuelmenama <manuel.mena090589@gmail.com>
2022-10-27 17:51:57 +0200

    added localFormat date

--- main.js
+++ main.js
@@ -75,3 +75,52 @@ const posts = [
     -stampare le iniziali del nome profilo e utilizzarle al posto della foto
 
 */
+
+const postList = document.getElementById("container");
+
+postListGenerator();
+
+//const likeButton = document.get
+
+function postListGenerator() {
+    postList.innerHTML = "";
+    posts.forEach(post => {
+
+        let convertDataFormat = new Date (post.created).toLocaleDateString("it-IT");
+
+        let postExample = `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${post.author.image}" alt="${post.author.image}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${convertDataFormat}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src="${post.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+        postList.innerHTML += postExample;
+
+    });
+};
